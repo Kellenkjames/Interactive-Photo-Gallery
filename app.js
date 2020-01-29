2647c7b69d36ca57fcc96420e9440653c0787742
Kellen <Kellen@Keiths-MBP.attlocal.net>
2020-01-28 21:50:29 -0800

    added each function to loop through data-title aka captions.

--- app.js
+++ app.js
@@ -2,7 +2,21 @@
 
 
 // Get the value of the search field whenever a user types in that field and log that value
-$('#mySearch').on('keyup', function (e) {
+$('#mySearch').on('keyup', function () {
     let searchValue = $('#mySearch').val();
-    console.log(searchValue);
+    console.log(searchValue.toLowerCase());
 });
+
+// Next, start trying to target the entire caption by using the getAttribute() method, 
+// then looping and logging all lowercase version of the captions to the console. 
+$('.item a').each(function () {
+    let caption = $(this).data("title");
+    console.log(caption);
+    // console.log( index + ": " + $( this ).text() + caption );
+
+});
+
+
+
+
+
