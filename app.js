3140311dee6d4684f36d05a095fb8855c42032a4
Kellen <Kellen@Keiths-MBP.attlocal.net>
2020-01-29 22:47:15 -0800

    added dataTitles empty array, working on comparing input against.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // HANDLE CONTENT FILTERING 
 
-
 // Store value of the search input
 const values = [];
+
+// Store captions
+const dataTitles = []; 
 
 function contentFilter() {
     
@@ -10,29 +12,32 @@ function contentFilter() {
     $('#mySearch').on('keyup', function () {
         let searchValue = $('#mySearch').val();
         // console.log(searchValue.toLowerCase());
-        // Push values to empty array
+        // Push values to values array
         values.push(searchValue);
         console.log(values);
     });
-    
+
     // Next, start trying to target the entire caption by using the getAttribute() method, 
     // then looping and logging all lowercase version of the captions to the console. 
     $('.item a').each(function () {
-        let caption = $(this).attr("data-title").toLowerCase();
+        let captions = $(this).attr("data-title").toLowerCase();
+        // console.log(captions);
+        
+        // Push captions to dataTitles array
+        dataTitles.push(captions);
 
         // Check if the current value of the search input is included within any of the captions
         // If so, log the associated image to the console. 
+        if (values.includes(dataTitles.toString())) {
+            console.log("what what, in the butt");
+        }
         
-    });
-    
-    
-    
-
-    
+    });    
 }
 
-
 contentFilter();
+
+console.log(dataTitles);
 
 
 
