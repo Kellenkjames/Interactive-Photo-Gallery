3dc3d04158adf9778eb0ce4cfa550e9c11d64d29
Kellen <Kellen@Keiths-MBP.attlocal.net>
2020-02-01 15:34:18 -0800

    search functionality complete with js / jQuery.

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-// HANDLE CONTENT FILTERING 
-
-// function contentFilter() {
-    
-//     // Get the value of the search field whenever a user types in that field and log that value
-//     $('#mySearch').on('keyup', function () {
-//         let value = $('#mySearch').val().toLowerCase();
-//         // Next, start trying to target the entire caption by using the getAttribute() method, 
-//         // then looping and logging all lowercase version of the captions to the console. 
-//         $('item a').filter(function () {
-//             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-//         });
-//         console.log(value);        
-//     }); 
-// };
-
-// contentFilter();
-
-// HANDLE CONTENT FILTERING 
+// HANDLE IMAGE FILTERING
 const searchBar = $('#mySearch');
 
-searchBar.addEventListener('keyup', function(e) {
-
+searchBar.on('keyup', function(e) {
+    
     // Grab the search term and convert to lower case.
-    const term = e.target.val().toLowerCase();
+    const term = e.target.value.toLowerCase();
+    
+    // Grab all of the captions, cycle through each one and log.
+    $('.item a').each(function () {
+        const caption = $(this).attr("data-title").toLowerCase();
+        
+        // If search term is in the caption, show the image/s associated to the caption.
+        if (caption.toLowerCase().indexOf(term) != -1) {
+            $(this).show();
+            
+        } // Otherwise, hide the image/s if the search term is not in the caption. 
+        else {
+            $(this).hide();
+        }
+        
+    });
+    
+}); 
 
-    // Target the entire caption 
-    const captions = $('ul').getElementsByTagName('li');
 
-    // Create an array
-    Array.from(captions).forEach(function(caption) {
-        const title = caption.
-    })
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
